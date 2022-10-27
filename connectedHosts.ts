e7b1775f330f11cc48c344c914943a96cd4aaeba
aouledissa <aouledissayousri@gmail.com>
2022-10-27 21:47:28 +0100

    finall version ready for deployment

--- connectedHosts.ts
+++ connectedHosts.ts
@@ -1,4 +1,4 @@
 export const HOSTS = [
    //"http://127.0.0.1:8000/core"
-   "https://e-recyclecoreservice-production.up.railway.app/"
+   "https://e-recyclecoreservice-production.up.railway.app/core"
 ]
