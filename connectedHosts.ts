9864dccfd4feb087cf8ed2617a5e656bdfc9f060
aouledissa <aouledissayousri@gmail.com>
2022-11-27 18:26:27 +0100

    final

--- connectedHosts.ts
+++ connectedHosts.ts
@@ -2,6 +2,5 @@ export const HOSTS = [
    //"http://127.0.0.1:8000/core",
    //"http://localhost:3000"
    "https://e-recyclecoreservice-production-ffdc.up.railway.app/core",
-   //"https://e-recycleservicegateway-production-788b.up.railway.app"
-   "http://localhost:3000"
+   "https://e-recycleservicegateway-production-788b.up.railway.app"
 ]
